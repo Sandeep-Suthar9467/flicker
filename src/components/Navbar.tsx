2fdf18a78b2b3b6ed37b74b13ce5ca9787ad78de
Laxman Ram <laxman.ram@zoro.co.uk>
2022-07-17 10:27:26 +0530

    login btn navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import Modal from '@mui/material/Modal';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import LoginForm from './LoginForm';
+import { useSelector } from 'react-redux';
 const style = {
   position: 'absolute' as 'absolute',
   top: '50%',
@@ -19,74 +20,78 @@ const style = {
   p: 4,
 }
 const ResponsiveAppBar = (): React.ReactElement => {
+  const isLoggedIn = useSelector((state: any) => state?.flicker?.isLoggedIn);
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   return (
     <>
-    <AppBar position="static" style={{}} >
-      <Container maxWidth="xl" style={{display:'flex',justifyContent: 'center',background: 'black'}}>
-        <Toolbar disableGutters  style={{display: 'flex' , width: '80%'}}>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              color: "#fff",
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
-          <Box sx={{ flexGrow: 0, marginLeft: "auto" }}>
-          <Button variant="contained" onClick={handleOpen}>Login</Button>
-          </Box>
-          {/* <Box sx={{ flexGrow: 0 }}>
+      <AppBar position="static" style={{}} >
+        <Container maxWidth="xl" style={{ display: 'flex', justifyContent: 'center', background: 'black' }}>
+          <Toolbar disableGutters style={{ display: 'flex', width: '80%' }}>
+            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                color: "#fff",
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            {
+              !isLoggedIn ?
+                <Box sx={{ flexGrow: 0, marginLeft: "auto" }}>
+                  <Button variant="contained" onClick={handleOpen}>Login</Button>
+                </Box>
+                : null
+            }
+            {/* <Box sx={{ flexGrow: 0 }}>
           <Button variant="contained">Sign Up</Button>
           </Box> */}
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-          >
+      >
         <Box sx={style}>
 
-        <LoginForm handleClose={handleClose}/>
+          <LoginForm handleClose={handleClose} />
         </Box>
 
       </Modal>
-  </>
+    </>
   );
 };
 export default ResponsiveAppBar;
